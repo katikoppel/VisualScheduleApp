a9cfe6b20b2e77578ba4e56c31fa330a67035196
katikoppel <katikoppel1995@gmail.com>
2026-04-26 20:30:01 +0300

    Add external Google login and update visual

--- VisualScheduleApp/Program.cs
+++ VisualScheduleApp/Program.cs
@@ -31,6 +31,13 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
 .AddEntityFrameworkStores<VisualScheduleAppContext>()
 .AddDefaultTokenProviders();
 
+builder.Services.AddAuthentication()
+    .AddGoogle(options =>
+    {
+        options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+        options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+    });
+
 builder.Services.Configure<EmailSettings>(
     builder.Configuration.GetSection("EmailSettings"));
 
